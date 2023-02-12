9efe2a3516598c67d0738567d2ca6cc111c54695
Josif Stojkov <josifstojkov@gmail.com>
2023-02-12 15:03:06 +0100

    JAVASCRIPT

---
+++ Homework JS 1/Homeworkjs1.js
@@ -0,0 +1,16 @@
+let name = "Josif Stojkov";
+let age = 23;
+let jobTitle = "Student";
+let gender = "male";
+let liveinSkopje = true;
+
+let personalDetails = {
+    name: name,
+    age: age,
+    jobTitle: jobTitle,
+    gender: gender,
+    liveinSkopje: liveinSkopje
+  };
+
+  console.log(personalDetails);
+
