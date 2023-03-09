9e1767934ba035d267b1ec572070959f5920d602
Josif Stojkov <josifstojkov@gmail.com>
2023-03-09 01:27:48 +0100

    ERR

--- Homework JS 7/HMJS7.1.js
+++ Homework JS 7/HMJS7.1.js
@@ -1,9 +1,19 @@
-let form2 = document.getElementById("myForm2");
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let phone = document.getElementById("phone");
-let address = document.getElementById("address");
-let zip = document.getElementById("zip");
+let form = document.getElementById('myForm');
 
-name.addEventListener("blur", function() {
-    if (!name.value) {
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let address = document.getElementById('address');
+let city = document.getElementById('city');
+
+name.addEventListener ('blur', (event) => {
+  if (name.value.trim() === '') {
+    name.classList.add('invalid');
+    document.getElementById('nameError').textContent = 'Please enter your name';
+
+  }
+}
